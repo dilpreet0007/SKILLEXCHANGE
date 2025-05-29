d335028b1c0ad4bba5c4a42ce7d4963f65a349df
dilpreet0007 <dilpreet.sandhu2004@gmail.com>
2025-05-29 19:39:16 +0530

    frontend env

--- frontend/src/utils/firebase.js
+++ frontend/src/utils/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "YOUR API",
-  authDomain: "Your Domain",
-  projectId: "YOUR ID",
-  storageBucket: "YOUR BUCKET", // ✅ FIXED
-  messagingSenderId: "YOUR ID",
-  appId: "YOUR APP ID",
-  measurementId: "YOUR measurementId "
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
