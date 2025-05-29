0cdb41838039f848330d3dd5f53d00c51d2abe56
dilpreet0007 <124757058+dilpreet0007@users.noreply.github.com>
2025-05-29 14:12:41 +0530

    Update firebase.js

--- frontend/src/utils/firebase.js
+++ frontend/src/utils/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB7VcQdwC1Zm3Y2PLzg9nMhuk9YerraNAM",
-  authDomain: "skillexchange-495ae.firebaseapp.com",
-  projectId: "skillexchange-495ae",
-  storageBucket: "skillexchange-495ae.appspot.com", // ✅ FIXED
-  messagingSenderId: "299366281095",
-  appId: "1:299366281095:web:83d6027772b7d753abad22",
-  measurementId: "G-10H9Q47RE2"
+  apiKey: "YOUR API",
+  authDomain: "Your Domain",
+  projectId: "YOUR ID",
+  storageBucket: "YOUR BUCKET", // ✅ FIXED
+  messagingSenderId: "YOUR ID",
+  appId: "YOUR APP ID",
+  measurementId: "YOUR measurementId "
 };
 
 const app = initializeApp(firebaseConfig);
